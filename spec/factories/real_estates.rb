40efef39e07dfbaa4a56290fecb1de78886510f0
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 21:21:08 +0200

    Modify real estate factory

--- spec/factories/real_estates.rb
+++ spec/factories/real_estates.rb
@@ -1,16 +1,15 @@
 FactoryBot.define do
   factory :real_estate do
-    street "MyString"
-    city "MyString"
-    zip "MyString"
-    state "MyString"
-    beds 1
-    baths 1
-    sq_ft 1
-    type ""
+    street Faker::Address.street_name
+    city Faker::Address.city
+    zip Faker::Address.zip_code
+    state Faker::Address.state
+    beds { rand(1..10) }
+    baths { rand(1..3) }
+    sq_ft { rand(50..10000) }
     sale_date "2018-01-13 16:19:06"
-    price "9.99"
-    latitude 1.5
-    longitude 1.5
+    price { rand(100..1_000_000) }
+    latitude Faker::Address.latitude
+    longitude Faker::Address.longitude
   end
 end
