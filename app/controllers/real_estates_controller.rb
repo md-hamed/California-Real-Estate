7593668ded390902706dbc00dca47c7f64cd5f77
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 23:30:33 +0200

    Add type, price, and sq_ft filters for real estates

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -5,9 +5,10 @@ class RealEstatesController < ApplicationController
 
   # GET /real_estates
   def index
-    @real_estates = RealEstate.all.page(params[:page])
+    @real_estates = RealEstate.filter(filtering_params)
+                              .page(params[:page])
 
-    render json: @real_estates
+    render json: { total_count: @real_estates.total_count, results: @real_estates }
   end
 
   # GET /real_estates/1
@@ -51,5 +52,11 @@ class RealEstatesController < ApplicationController
       params.require(:real_estate).permit(:street, :city, :zip, :state, 
                                           :beds, :baths, :sq_ft, :building_type,
                                           :sale_date, :price, :longitude, :latitude)
+    end
+
+    # Params that we will filter upon
+    def filtering_params
+      params.slice(:type, :min_price, :max_price, 
+                   :min_sq_ft, :max_sq_ft)
     end
 end
