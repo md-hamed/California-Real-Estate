c3efe700eed03b3126f652e65fef89dfe390f5d8
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 18:54:04 +0200

    Modify real estate long and lat validation to allow nil

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -6,8 +6,12 @@ class RealEstate < ApplicationRecord
   validates :street, :city, :zip, :state, :sq_ft, :building_type,
             :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-  validates :latitude , numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to:  90 }
-  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  validates :latitude , numericality: { greater_than_or_equal_to: -90, 
+                                        less_than_or_equal_to: 90,
+                                        allow_nil: true }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180,
+                                        less_than_or_equal_to: 180,
+                                        allow_nil: true }
 
   # aliases
   alias_attribute :type, :building_type
