b7b4b7f9c28953d2b0551e108994ac0d858772aa
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 17:58:36 +0200

    Add basic validation for real estate fields

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,3 +1,12 @@
 class RealEstate < ApplicationRecord
-  enum building_type: [:unkown, :residential, :condo, :multi_family]
+  # enums
+  enum building_type: [:unknown, :residential, :condo, :multi_family]
+  
+  # validations
+  validates :street, :city, :zip, :state, :sq_ft, :building_type,
+            :price, presence: true
+  validates :price, numericality: { greater_than: 0, only_integer: true }
+  validates :latitude , numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to:  90 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
 end
