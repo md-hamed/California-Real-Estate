d7b5c24a8fee82ae2e897c516f7d4108395609f8
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 17:59:26 +0200

    Add alias of real estate type for building_type

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,7 +1,7 @@
 class RealEstate < ApplicationRecord
   # enums
   enum building_type: [:unknown, :residential, :condo, :multi_family]
-  
+
   # validations
   validates :street, :city, :zip, :state, :sq_ft, :building_type,
             :price, presence: true
@@ -9,4 +9,6 @@ class RealEstate < ApplicationRecord
   validates :latitude , numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to:  90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
+  # aliases
+  alias_attribute :type, :building_type
 end
