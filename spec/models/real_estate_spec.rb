4578d5d00287d5dac0d34139ec5ee3fbb1de306d
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 19:55:28 +0200

    Modify real estate model specs

--- spec/models/real_estate_spec.rb
+++ spec/models/real_estate_spec.rb
@@ -12,7 +12,17 @@ RSpec.describe RealEstate, type: :model do
 
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   it { is_expected.to validate_numericality_of(:latitude).is_greater_than_or_equal_to(-90)
-                                                         .is_less_than_or_equal_to(90) }
+                                                         .is_less_than_or_equal_to(90)
+                                                         .allow_nil }
   it { is_expected.to validate_numericality_of(:longitude).is_greater_than_or_equal_to(-180)
-                                                          .is_less_than_or_equal_to(180) }
+                                                          .is_less_than_or_equal_to(180)
+                                                          .allow_nil }
+  it { is_expected.to validate_numericality_of(:beds).is_greater_than_or_equal_to(0)
+                                                      .allow_nil
+                                                      .only_integer }
+  it { is_expected.to validate_numericality_of(:baths).is_greater_than_or_equal_to(0)
+                                                      .allow_nil
+                                                      .only_integer }
+  it { is_expected.to validate_numericality_of(:sq_ft).is_greater_than_or_equal_to(0)
+                                                      .only_integer }
 end
