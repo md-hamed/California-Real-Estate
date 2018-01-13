73bc7dee6e7903e77f10893787b25e65cb0236e5
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 18:13:15 +0200

    Add model specs for real estate

--- spec/models/real_estate_spec.rb
+++ spec/models/real_estate_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe RealEstate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # validation specs
+  it { is_expected.to validate_presence_of :street }
+  it { is_expected.to validate_presence_of :city }
+  it { is_expected.to validate_presence_of :zip }
+  it { is_expected.to validate_presence_of :state }
+  it { is_expected.to validate_presence_of :sq_ft }
+  it { is_expected.to validate_presence_of :building_type }
+  it { is_expected.to validate_presence_of :price }
+
+  it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+  it { is_expected.to validate_numericality_of(:latitude).is_greater_than_or_equal_to(-90)
+                                                         .is_less_than_or_equal_to(90) }
+  it { is_expected.to validate_numericality_of(:longitude).is_greater_than_or_equal_to(-180)
+                                                          .is_less_than_or_equal_to(180) }
 end
