bc4437de9671f7c9644ecac04ff225d8af4dbece
md-hamed <mohamed.hamed@espace.com.eg>
2018-01-13 18:30:37 +0200

    Add real estates controller

---
+++ app/controllers/real_estates_controller.rb
@@ -0,0 +1,51 @@
+class RealEstatesController < ApplicationController
+  before_action :set_real_estate, only: [:show, :update, :destroy]
+
+  # GET /real_estates
+  def index
+    @real_estates = RealEstate.all
+
+    render json: @real_estates
+  end
+
+  # GET /real_estates/1
+  def show
+    render json: @real_estate
+  end
+
+  # POST /real_estates
+  def create
+    @real_estate = RealEstate.new(real_estate_params)
+
+    if @real_estate.save
+      render json: @real_estate, status: :created, location: @real_estate
+    else
+      render json: @real_estate.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /real_estates/1
+  def update
+    if @real_estate.update(real_estate_params)
+      render json: @real_estate
+    else
+      render json: @real_estate.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /real_estates/1
+  def destroy
+    @real_estate.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_real_estate
+      @real_estate = RealEstate.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def real_estate_params
+      params.fetch(:real_estate, {})
+    end
+end
